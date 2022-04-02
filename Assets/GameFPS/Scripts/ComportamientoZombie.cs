447f2c967608b83209dc922252a46b625477eac9
Manuel E. Hernandez <kicke10h@gmail.com>
2022-04-01 19:23:44 -0600

    third commit

---
+++ Assets/GameFPS/Scripts/ComportamientoZombie.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamientoZombie : MonoBehaviour
+{
+    private int saludActual;
+    public int salud = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        saludActual = salud;
+    }
+
+    public void OnCollisionEnter(Collision colision)
+    {
+        if(saludActual > 0)
+        {
+            RegistrarDanio(UnityEngine.Random.Range(5,20));
+        }
+    }
+
+
+    private void RegistrarDanio(int danioARegistrar)
+    {
+        saludActual -= danioARegistrar;
+        if(saludActual <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+    
+}
--- Assets/GameFPS/Scripts/pistolaDeMano.cs
+++ Assets/GameFPS/Scripts/pistolaDeMano.cs
@@ -3,7 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK.Examples;
 
-public class NewBehaviourScript : GunShoot
+public class PistolaDeMano : GunShoot
 {
-  
+    public Animator anim;
+    public GameObject flashPrefab;
+    public AudioClip sonidoDisparo;
+
+  protected override void FireProjectile()
+    {
+        base.FireProjectile();
+        anim.SetTrigger("Fire");
+        var muzzleFlash = Instantiate(flashPrefab,
+            projectileSpawnPoint.position,
+            projectileSpawnPoint.rotation);
+        Destroy(muzzleFlash, 0.3f);
+
+        AudioSource.PlayClipAtPoint(sonidoDisparo, transform.position);
+    }
 }
